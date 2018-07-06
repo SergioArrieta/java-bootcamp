c8062eee310f64a62ac1f84b3e7fdcde6ae6cbcd
Sergio <arrietasergio94@gmail.com>
2018-07-06 18:03:45 -0300

    junit test

---
+++ firstMaven/src/test/java/firstMaven/TestMain.java
@@ -0,0 +1,14 @@
+package firstMaven;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestMain extends TestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
